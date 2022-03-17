ba6dedc0767a8131e003911d51b8d3d563e0f468
JuanPineda115 <pin19087@uvg.edu.gt>
2022-03-16 19:29:12 -0600

    regresion

--- src/LoadData.py
+++ src/LoadData.py
@@ -1,13 +1,13 @@
-import sklearn as skl
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 # Lo mejor seria que este metodo devuelva de una vez los datos de entrenamiento y de prueba separados
-def load_regresion_data():
+def load_KNNSVM_data():
   df = pd.read_csv('../data/Oranges vs Grapefruit.csv')
-  train, test = skl.train_test_split(df, test_size=0.7)
+  train, test = train_test_split(df, test_size=0.3)
   return df, train, test
 
-def load_KNNSVM_data():
+def load_regresion_data():
   df = pd.read_csv('../data/Walmart.csv')
-  train, test = skl.train_test_split(df, test_size=0.7)
+  train, test = train_test_split(df, test_size=0.3)
   return df, train, test
---
+++ src/PR.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import LoadData
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+
+ds, train, test = LoadData.load_regresion_data()
+ds = pd.DataFrame(ds)
+
+# Variables independientes: Weekly_Sales, Unemployment, Fuel_Price, CPI
+# Variables dependetientes: Date
+# Variable a predecir Weekly_Sales
+ds['Date'] = pd.to_datetime(ds['Date'], dayfirst=True,)
+ds['Week'] = ds['Date'].dt.week
+y = ds.iloc[:, 2].values
+x = ds.iloc[:, 0:1].values
+
+train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.3)
+
+polinomio = PolynomialFeatures(degree=7)
+
+# Se transforma las caracteristicas
+poli_xTr = polinomio.fit_transform(train_x)
+poli_xT = polinomio.fit_transform(test_x)
+
+pr = linear_model.LinearRegression()
+pr.fit(poli_xTr, train_y)
+Y_pred_pr = pr.predict(poli_xT)
+
+plt.scatter(test_x, test_y)
+plt.plot(test_x, Y_pred_pr, color='green', linewidth=3)
+plt.show()
+
+print('Regresion polinomial\n')
+print('Valor de "a":\n')
+print(pr.coef_)
+print('Valor de "b":\n')
+print(pr.intercept_)
+print('Precisión:\n')
+print(pr.score(poli_xTr, train_y))
