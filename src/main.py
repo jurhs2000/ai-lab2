a1473388308cd8b51a4e375bbc76b79cbd57fe09
Julio Herrera <42546972+jurhs2000@users.noreply.github.com>
2022-03-16 18:03:43 -0600

    feat: KNN algoritmo, score y graficas

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
-from LoadData import load_data_PR_KNN
+from LoadData import load_data_KNN
+from KNN import KNN
 
-print(load_data_PR_KNN())
+# return a pandas dataframe
+knn_train, knn_test = load_data_KNN()
+knn = KNN(3, knn_train, knn_test)
+
+predictions = knn.predict(knn.x_test)
+
+print(knn.accuracy(predictions))
+
+knn.graph_confusion_matrix(predictions)
+
+knn.graph_train_data(predictions)
+knn.graph_hsv_rgb(predictions)
